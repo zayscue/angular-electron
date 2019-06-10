1590a40bd9679e996cf48b9bd80d733272c68b7b
Ayscue, Zackery T <zackery.ayscue@cengage.com>
2019-06-10 16:42:30 -0400

    Initial Commit

---
+++ electron/dist/main.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var electron_1 = require("electron");
+var path = require("path");
+var url = require("url");
+var fs = require("fs");
+var win;
+electron_1.app.on('ready', createWindow);
+electron_1.app.on('activate', function () {
+    if (win === null) {
+        createWindow();
+    }
+});
+function createWindow() {
+    win = new electron_1.BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: { nodeIntegration: true }
+    });
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, "/../../dist/angular-electron/index.html"),
+        protocol: 'file:',
+        slashes: true
+    }));
+    win.webContents.openDevTools();
+    win.on('closed', function () {
+        win = null;
+    });
+}
+electron_1.ipcMain.on('getFiles', function (event, arg) {
+    var files = fs.readdirSync(__dirname);
+    win.webContents.send('getFilesResponse', files);
+});
+//# sourceMappingURL=main.js.map
